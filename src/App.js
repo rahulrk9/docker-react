decace9150d4364243b31608ef82832e3c6f40c2
rahulrk9 <rahulrkn@yahoo.com>
2019-05-19 13:34:18 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,15 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Hope it works nows.
         </p>
+        <p>Silly parenthesis</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, Sure I will !! :) ,Done 
         </a>
       </header>
     </div>
